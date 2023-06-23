d2c294dd0b3c277e13a218436a8aae62e8992a4f
richalsthomask <richalsthomask@gmail.com>
2023-06-23 18:14:16 +0530

    added apis for tasks and tasks history operation

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,22 +1,44 @@
-"""task_manager URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.urls import path
+from rest_framework.routers import SimpleRouter
 
+from tasks.apiViews import TaskViewSet, TaskHistoryViewSet
+from django.contrib.auth.views import LogoutView
+from tasks.views import (
+    delete_task,
+    redirect_to_login,
+    report_view,
+    delete_all,
+    GenericTaskVew,
+    GenericTaskCreateView,
+    GenericTaskUpdateView,
+    GenericTaskDeleteView,
+    UserCreateView,
+    UserLoginView,
+    GenericTaskDetailView,
+    GenericCompletedTasksView,
+)
+
+router = SimpleRouter()
+
+router.register("api/task", TaskViewSet)
+router.register("api/task_history", TaskHistoryViewSet)
 
 urlpatterns = [
+    path("", redirect_to_login),
     path("admin/", admin.site.urls),
-]
+    path("signup", UserCreateView.as_view()),
+    path("login", UserLoginView.as_view()),
+    path("logout", LogoutView.as_view()),
+    # Add all your views here
+    path("tasks/", GenericTaskVew.as_view()),
+    path("create_task", GenericTaskCreateView.as_view()),
+    path("update_task/<pk>", GenericTaskUpdateView.as_view()),
+    path("delete_task/<pk>", GenericTaskDeleteView.as_view()),
+    path("delete-task/<int:task_id>/", delete_task),
+    path("task/<pk>", GenericTaskDetailView.as_view()),
+    path("completed_tasks/", GenericCompletedTasksView.as_view()),
+    path("all_tasks/", report_view),
+    path("delete_all/", delete_all),
+] + router.urls
